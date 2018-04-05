2efaba3536e86a51c5957958b8eba94b3c8bd1ca
Jack Bai <yangpire@gmail.com>
2018-04-05 18:15:40 -0400

    p5update

--- assignments/p5/js/sketch.js
+++ assignments/p5/js/sketch.js
@@ -1,48 +1,92 @@
 // ***** Global variables ***** //
-var FARTable;
+var yearVsFar;
 var topY = 50;
-var bottomY = 500;
+var bottomY = 800;
 var leftX = 50;
-var rightX = 700;
+var rightX = 1000;
 var textLeft = 30;
-var tableofFAR = new p5.Table;
+var delay;
 
-// ***** Preload function ***** //
+// ***** Preload function ***** /
+
 function preload(){
-  yearVsFar = loadTable('data/yearVsFar.csv', 'csv', 'header');
+  yearVsFar = loadTable('../data/yearVsFar.csv', 'csv', 'header');
   console.log('Done loading table...');
 }
 
-// ***** Setup function ***** //
+// ***** setup function***** //
 function setup(){
-  createCanvas(1000, 800);
-  textSize(12);
+  createCanvas(1100, 900);
+  textSize(16);
   textFont('Roboto');
   console.log('Setup complete...');
-  print(FARTable.getRowCount() + ' rows loaded...');
-  print(FARTable.getColumnCount() + ' columns loaded...');
+  print(yearVsFar.getRowCount() + ' rows loaded...');
+  print(yearVsFar.getColumnCount() + ' columns loaded...');
+  colorMode(HSB, 255);
   noLoop();
-  createNewTable();
 }
 
-background(84);
- // fill(255);
-// Draw horizontal lines and the y axis
- for (var i = 0; i < 24; i++){
-   noStroke();
-   textAlign(RIGHT, CENTER);
-   text(round(map(i, 0, 23, 1900, 2015)), textLeft, map(i, 0, 23, bottomY, topY));
-   stroke(70);
-   line(textLeft + 10, map(i, 0, 23, bottomY, topY), rightX + 10, map(i, 0, 23, bottomY, topY));
-
-}
-   noStroke();
-
-
-  // Draw vertical lines and the x axis
-  for (var i = 0; i < 15; i++) {
-    textAlign(CENTER, TOP);
-    text(round(map(i, 0, 8, 0, 30)), map(i, 0, 8, leftX, rightX), bottomY + 5);
+ // Draw horizontal lines and the y axis
+function draw(){
+  background(255);
+  // fill(255);
+  for (var i = 0; i < 24; i++){
+    noStroke();
+    textAlign(RIGHT, CENTER);
+    text(round(map(i, 0, 23, 1900, 2015)), textLeft, map(i, 0, 23, bottomY, topY));
     stroke(70);
-    line(map(i, 0, 8, 40, 1010), topY , map(i, 0, 8, 40, 1010), bottomY);
+    line(textLeft + 10, map(i, 0, 23, bottomY, topY), rightX + 10, map(i, 0, 23, bottomY, topY));
   }
+    noStroke();
+    // Draw vertical lines and the x axis
+   for (var i = 0; i < 15; i++) {
+     textAlign(CENTER, TOP);
+     text(round(map(i, 0, 8, 0, 50)), map(i, 0, 8, leftX, rightX), bottomY + 5);
+     stroke(70);
+     line(map(i, 0, 8, 40, 1010), topY , map(i, 0, 8, 40, 1010), bottomY);
+   }
+
+   //draw dots
+
+
+
+   for(var i = 0; i < yearVsFar.getRowCount(); i++){
+     var BuiltFarColumn = yearVsFar.getNum(i, 'BuiltFAR');
+     var ResidFarColumn = yearVsFar.getNum(i, 'ResidFAR');
+     var CommFarColumn = yearVsFar.getNum(i,'CommFAR');
+     var FacilFarColumn = yearVsFar.getNum(i,'FacilFAR');
+     var YearOfData = yearVsFar.getNum(i,'YearBuilt')
+     var yearPosition = map(YearOfData,1900,2015, bottomY,topY);
+     var BuiltFarPosition = map(BuiltFarColumn,0,25,leftX,rightX)
+     var ResidFarPosition = map(ResidFarColumn,0,25,leftX,rightX)
+     var CommFarPosition = map(CommFarColumn,0,50,leftX,rightX)
+     var FacilFarPosition = map(FacilFarColumn,0,50,leftX,rightX)
+
+
+
+      // If year is less than 1900 or numFAR is greater is 10
+      if (YearOfData >= 1900 ) {
+       // map each FAR between leftX and rightX
+       var numBuiltFar = BuiltFarPosition
+       var numResidFar = ResidFarPosition
+       var numCommFar = CommFarPosition
+       var numFacilFar = FacilFarPosition
+       // map each yearbuilt between bottomY and topY
+       var YearOfData = map(YearOfData, 1900, 2014, bottomY, topY);
+       fill('#1C4C56');
+       noStroke();
+       ellipse(numBuiltFar, YearOfData, 5, 5);
+
+       fill('#CFB276');
+       ellipse(numResidFar, YearOfData, 6, 6);
+
+       fill('#6B0803')
+       ellipse(numCommFar, YearOfData,6 ,6);
+
+       fill('#B95F21')
+       ellipse(numFacilFar, YearOfData,6 ,6);
+
+
+}
+       }
+   }
